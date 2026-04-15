9129683457b34f57b0ea8679f14931243fbcbca8
Juliovl <juliocvilaca4@gmail.com>
2026-04-15 20:02:56 -0300

    up exercise

--- Js Luiz Otavio/aulas 3/exercicios/ex001.js
+++ Js Luiz Otavio/aulas 3/exercicios/ex001.js
@@ -1,1 +1,2 @@
-console.log('Meu nome é Júlio. E estou codando as 10h da manha')
+console.log('Meu nome é Júlio. E estou codando as 19:36h da manha'); 
+//caso eu precisar isolar o número me basta somente eu fazer um time de virgula da aspas.("didio",10);
